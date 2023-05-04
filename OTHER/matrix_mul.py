9e3a37c26afea1b4ddc319617c6b1eaf8483770c
Ahmed Sufyan <sufyandaredevil@gmail.com>
2023-05-04 19:02:43 +0530

    added venv to gitignore

--- OTHER/matrix_mul.py
+++ OTHER/matrix_mul.py
@@ -1,16 +1,19 @@
-A = [[1, 0, 2, 1], [4, 1, 0, 0], [0, 1, 3, 0], [5, 0, 2, 1] ]
+A = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    ]
 
-B = [[0, 1, 0, 1], [2, 1, 0, 4], [2, 0, 1, 1], [1, 3, 5, 0]]
+B = [
+    [9,8,7],
+    [6,5,4],
+    [3,2,1],
+    ]
 
-result = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]
+result = [[0,0,0],[0,0,0],[0,0,0]]
 
-# iterating by row of A
 for i in range(len(A)):
-
-    # iterating by column by B
     for j in range(len(B[0])):
-
-        # iterating by rows of B
         for k in range(len(B)):
             result[i][j] += A[i][k] * B[k][j]
 
